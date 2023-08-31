8cdb28939716813075a01f9625027b49fe293127
Kang Hsu <popkart13@gmail.com>
2023-08-31 21:21:47 +0800

    Initial Commit

---
+++ Ray_Tracing_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ray_Tracing_1
+//
+//  Created by 許熙康 on 2023/8/31.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
